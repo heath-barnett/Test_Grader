e4898de526d092acf3a0b5781ae08a79fcd6d82e
Heath Barnett <heath.barnett@gmail.com>
2017-02-11 17:42:04 -0600

    first

--- grade.py
+++ grade.py
@@ -1,17 +1,32 @@
 __author__ = 'heath'
-import sys, os, csv, jinja2
+import sys, os, csv, jinja2, glob
 import pandas as pd
 import numpy as np
 import sqlite3
 
 # Location of file
-Location = 'data/results_grid.xlsx'
+Location = 'data/Exam_I_Results_Grid.xlsx'
 
 # Parse a specific sheet
 df = pd.read_excel(Location,0,converters={'ID Number':str})
 keys = df.loc[:1,'Q 1':]
 results = df.loc[2:]
+results = results.assign(Total=round(results['Short Answer']+results['# Correct']/30*40,2).values)
+tavg = round(results['Total'].mean(),2)
+tmin = results['Total'].min()
+tmax = results['Total'].max()
 
+sam = round(results['Short Answer'].mean(),2)
+samin = results['Short Answer'].min()
+samax = results['Short Answer'].max()
+
+sam = round(results['Short Answer'].mean(),2)
+samin = results['Short Answer'].min()
+samax = results['Short Answer'].max()
+
+mcm = round(results['# Correct'].mean(),2)
+mcmin = results['# Correct'].min()
+mcmax = results['# Correct'].max()
 # Change the default delimiters used by Jinja such that it won't pick up brackets attached to LaTeX macros.
 report_renderer = jinja2.Environment(
     block_start_string='\BLOCK{',
@@ -27,17 +42,19 @@ report_renderer = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.abspath('.'))
 )
 
-for i, row in results.iterrows():
+for i, row in df.loc[2:].iterrows():
     template = report_renderer.get_template('templates/report_template.tex')
     name = row['Student Name']
     cwid = str(row['ID Number'])
     exam = 'Exam I'
     keyname = row['Key Name']
-    score = row['Score']
+    sans = row['Short Answer']
     correct = row['# Correct']
-    blank = row['Blank Count']
-
+    total = format(sans + correct/30*40,'.2f')
+    blank = str(row['Blank Count'])
+    cid = 'Chemistry 1007'
     df1 = row['Q 1':]
+    max = len(df1.index)
     key = row['Key Name']
     if key == 'A':
         keyid = 0
@@ -45,20 +62,28 @@ for i, row in results.iterrows():
         keyid = 1
     ans = keys.ix[keyid]
     gradeTable = pd.concat([df1, ans], axis=1)
-    gradeTable.columns = ['User Response', 'Correct Response']
-    gradeTable = pd.DataFrame(gradeTable).to_latex()
+    gradeTable.columns = ['Response', 'Correct']
+    gradeTable = pd.DataFrame(gradeTable).to_latex(bold_rows=True, column_format='rcc')
 
     filename = str(name).replace(" ", "_") + '_results.tex'
     folder = 'output'
     outpath = os.path.join(folder, filename)
     outfile = open(outpath, 'w')
-    outfile.write(template.render(name=name, cwid=cwid, exam=exam, keyname=keyname, score=score, correct=correct, gradeTable=gradeTable))
+    outfile.write(template.render(name=name, cid=cid, tavg=tavg, tmin=tmin, tmax=tmax, sam=sam, samin=samin, samax=samax, mcm=mcm, mcmin=mcmin,mcmax=mcmax, max=max,cwid=cwid, total=total, exam=exam, keyname=keyname, sans=sans, blank=blank, correct=correct, gradeTable=gradeTable))
     outfile.close()
-    os.system('pdflatex -output-directory=' + folder + " " + outpath)
-    #os.system("pdflatex -outpu\\t-directory=" + folder + " " + outpath)
+    os.system('pdflatex -quiet -output-directory=' + folder + " " + outpath)
+    #for OS X / Linux
+    #os.system("pdflatex -output-directory=" + folder + " " + outpath)
     if i ==3:
         break
-#os.system("rm "+ folder +"/*.tex")
-#os.system("rm "+ folder +"/*.aux")
-#os.system("rm "+ folder +"/*.log")
+os.chdir(folder)
+for filename in glob.glob('*.tex'):
+    os.remove( filename )
+for filename in glob.glob('*.aux'):
+    os.remove( filename )
+for filename in glob.glob('*.log'):
+    os.remove( filename )
+os.chdir('..')
+
+
 print('Grader is finished.')
